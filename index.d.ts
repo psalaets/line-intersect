c50e97eebf42f70aacb8873e2c242f73d1fbd550
Sean S. LeBlanc <sean.s.leblanc@gmail.com>
2021-02-03 01:03:28 -0500

    fix: improve specificity of `IntersectionCheckResult` type
    `point` is only defined when the result is `intersecting`, and is guaranteed to be defined in this case; e.g.
    ```ts
    const i = checkIntersection(...);
    if (i.type === 'intersecting') {
    	i.point; // valid TS
    } else {
    	i.point; // Property 'point' does not exist
    }
    ```

--- index.d.ts
+++ index.d.ts
@@ -4,10 +4,14 @@ declare module 'line-intersect' {
     y: number
   }
 
-  interface IntersectionCheckResult {
-    type: 'none' | 'parallel' | 'colinear' | 'intersecting',
-    point?: Point
-  }
+  type IntersectionCheckResult =
+    | {
+        type: 'none' | 'parallel' | 'colinear';
+      }
+    | {
+        type: 'intersecting';
+        point: Point;
+      };
 
   function checkIntersection(x1: number, y1: number, x2: number, y2: number, x3: number, y3: number, x4: number, y4: number): IntersectionCheckResult;
   function colinearPointWithinSegment(pointX: number, pointY: number, startX: number, startY: number, endX: number, endY: number): boolean;
